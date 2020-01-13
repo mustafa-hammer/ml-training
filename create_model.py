825038d3b4f616a916057c8fac2af5b0da593633
mustafa-hammer <elhilo23@gmail.com>
2020-01-13 12:12:48 -0500

    additional cleanup

--- create_model.py
+++ create_model.py
@@ -5,20 +5,18 @@ from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
 import pandas as pd
 
-
 # Import Data
-# you can switch this to "questions.csv" and then rerun to generate a new model. small_list is a subset to speed up development. 
-data=pd.read_csv('./statements.csv')
+# you can switch this to "statements_long.csv" but you will need to perform some basic ETL on it as the model will fail to create. 
+data=pd.read_csv('./statements_small.csv')
 
 q1_list = data['question1'].values.tolist()
 
-#TODO: cleanup the data by removing nulls, extra characters etcs. The data needs to go through an ETL process which im skipping at the moment. 
+#TODO: cleanup the data by removing nulls, extra characters etcs. The data needs to go through a basic ETL process which im skipping at the moment. 
 
 tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(q1_list)]
 
-print(tagged_data)
+#print(tagged_data)
 
-#TODO understand these numbers
 max_epochs = 100
 vec_size = 20
 alpha = 0.025
@@ -42,10 +40,5 @@ for epoch in range(max_epochs):
     # fix the learning rate, no decay
     model.min_alpha = model.alpha
 
-model.save("d2v.model")
+model.save("compare_d2v.model")
 print("Model Saved")
-
-#helpfull links:
-# https://medium.com/@mishra.thedeepak/doc2vec-simple-implementation-example-df2afbbfbad5
-# https://kanoki.org/2019/03/07/sentence-similarity-in-python-using-doc2vec/
-# https://towardsdatascience.com/multi-class-text-classification-with-doc2vec-logistic-regression-9da9947b43f4
